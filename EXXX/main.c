3060442006b4154a3a67c005bfab04ae45fcf22e
alic3 <alic3dev@gmail.com>
2024-10-12 14:23:32 -0400

    Initial Commit

---
+++ EXXX/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  EXXX
+//
+//  Created by Alice Grace on 10/12/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
